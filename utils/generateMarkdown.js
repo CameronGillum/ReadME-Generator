8a78a80f364b0dabd67e93aaa3de9272f1735031
CameronGillum <GillumTCameron@gmail.com>
2024-07-05 12:08:58 -0400

    Adds function to generate markdown

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -30,12 +30,47 @@ function renderLicenseLink(license) {
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (!license) {
+    return '';
+  }
+  return `## License
+This project is licensed under the ${license} License - see the [LICENSE](${renderLicenseLink(license)}) file for details.`;
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
 
+${renderLicenseBadge(data.license)}
+
+## Description
+${data.description}
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [License](#license)
+- [Questions](#questions)
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+
+## Contributing
+${data.contribution}
+
+## Tests
+${data.tests}
+
+${renderLicenseSection(data.license)}
+
+## Questions
+If you have any questions about the project, you can contact me at [${data.email}](mailto:${data.email}). You can also find more of my work on my [GitHub](https://github.com/${data.github}).
 `;
 }
 
