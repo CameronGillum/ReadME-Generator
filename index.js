75329550ebfe69c5b79aae2e7129a44810fed247
CameronGillum <GillumTCameron@gmail.com>
2024-07-05 12:10:22 -0400

    Updates question in index.js

--- index.js
+++ index.js
@@ -5,48 +5,53 @@ const generateMarkdown = require('./utils/generateMarkdown');
 
 // Array of questions for user input
 const questions = [
-  {
-    type: 'input',
-    name: 'title',
-    message: 'What is the title of your project?',
-  },
-  {
-    type: 'input',
-    name: 'description',
-    message: 'Provide a description of your project:',
-  },
-  {
-    type: 'input',
-    name: 'installation',
-    message: 'What are the installation instructions?',
-  },
-  {
-    type: 'input',
-    name: 'usage',
-    message: 'What is the usage information?',
-  },
-  {
-    type: 'input',
-    name: 'contribution',
-    message: 'What are the contribution guidelines?',
-  },
-  {
-    type: 'input',
-    name: 'tests',
-    message: 'What are the test instructions?',
-  },
-  {
-    type: 'input',
-    name: 'github',
-    message: 'Enter your GitHub username:',
-  },
-  {
-    type: 'input',
-    name: 'email',
-    message: 'Enter your email address:',
-  }
-];
-
+    {
+      type: 'input',
+      name: 'title',
+      message: 'What is the title of your project?',
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Provide a description of your project:',
+    },
+    {
+      type: 'input',
+      name: 'installation',
+      message: 'What are the installation instructions?',
+    },
+    {
+      type: 'input',
+      name: 'usage',
+      message: 'What is the usage information?',
+    },
+    {
+      type: 'input',
+      name: 'contribution',
+      message: 'What are the contribution guidelines?',
+    },
+    {
+      type: 'input',
+      name: 'tests',
+      message: 'What are the test instructions?',
+    },
+    {
+      type: 'list',
+      name: 'license',
+      message: 'Choose a license for your project:',
+      choices: ['MIT', 'GPLv3', 'Apache 2.0', 'BSD 3-Clause', 'None']
+    },
+    {
+      type: 'input',
+      name: 'github',
+      message: 'Enter your GitHub username:',
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: 'Enter your email address:',
+    }
+  ];
 // Function to write README file
 function writeToFile(fileName, data) {
   fs.writeFile(fileName, data, (err) =>
